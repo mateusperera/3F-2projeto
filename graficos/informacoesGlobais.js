13c1a8d078e1ab119a0cb91e172081cb37a666d6
mateusperera <110178817+mateusperera@users.noreply.github.com>
2024-10-04 08:55:09 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,9 +3,17 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizaDadosGlobais(){
     const res = await fetch(url)
     const dados = await res.json ()
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+    const tempoMedio = dados.tempo_medio
     console.log(dados)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-texto')
     const container = document.getElementById('graficos-container')
+    paragrafo.innerHTML = `No mundo tem <span> ${totalPessoasMundo} bilhoes </span> de 
+    habitantes, dos quais <span> ${totalPessoasConectadas} bilhoes </span> estao conectadas em alguma 
+    rede social por um tempo medio de <span> ${tempoMedio} horas</span>.`
+    container.appendChild(paragrafo)
 }
 visualizaDadosGlobais();
