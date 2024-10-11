d74de480d236426fddcf49d249dcf8a862fc9eb2
mateusperera <110178817+mateusperera@users.noreply.github.com>
2024-10-11 08:52:28 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,19 +1,20 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function visualizaDadosGlobais(){
-    const res = await fetch(url)
-    const dados = await res.json ()
+    const res = await fetch(url);
+    const dados = await res.json();
     const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
     const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
-    const tempoMedio = dados.tempo_medio
-    console.log(dados)
+    const tempoMedio = dados.tempo_medio;
+    console.log(dados);
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-texto')
     const container = document.getElementById('graficos-container')
-    paragrafo.innerHTML = `No mundo tem <span> ${totalPessoasMundo} bilhoes </span> de 
-    habitantes, dos quais <span> ${totalPessoasConectadas} bilhoes </span> estao conectadas em alguma 
-    rede social por um tempo medio de <span> ${tempoMedio} horas</span>.`
-    container.appendChild(paragrafo)
+    paragrafo.innerHTML = `O mundo tem <span> ${totalPessoasMundo} bilhões </span> de 
+    habitantes, dos quais <span> ${totalPessoasConectadas} bilhões </span> estão conectados
+    em alguma rede social por um tempo médio de <span> ${tempoMedio} horas </span>.`
+    container.appendChild(paragrafo);
 }
+
 visualizaDadosGlobais();
--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -11,5 +11,12 @@ const infos = [
         y: quantidadeDeUsuarios,
         type: 'bar'
     }
-]
+  ]
+  
+const grafico = document.createElement('div');
+grafico.className = 'grafico';
+document.getElementById('graficos-container').appendChild(grafico);
+Plotly.newPlot(grafico,infos);
 }
+
+quantidadeDeUsuarios();
