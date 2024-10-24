d40f905fab358aed0d405f3439f935ab70364729
mateusperera <110178817+mateusperera@users.noreply.github.com>
2024-10-24 09:24:06 -0300

    Add files via upload

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,26 +1,39 @@
 import { getCSS } from "./comum.js";
 async function quantidadeDeUsuarios(){
-const  url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json"
-const res = await fetch(url);
-const dados = await res.json();
-const nomeDasRedes = Object.keys(dados);
-const quantidadeDeUsuarios = Object.values(dados);
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url);
+    const dados = await res.json();
+    const nomeDasRedes = Object.keys(dados);
+    const quantidadeDeUsuarios = Object.values(dados);
 
-const infos = [
-    {
-        x: nomeDasRedes,
-        y: quantidadeDeUsuarios,
-        type: 'bar',
-        marker: {
-          color: getCSS("--cor-primaria")
+    const infos = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeDeUsuarios,
+            type: 'bar',
+            marker:{
+                color: getCSS('--cor-primaria')
+            }
         }
+    ]
+    const layout = {
+      plot_bgcolor: getCSS('--cor-de-fundo'),
+      paper_bgcolor:getCSS('--cor-de-fundo'),
+      title: {
+        Text: 'Redes sociais com mais usuarios no mundo',
+        x: 0,
+        font:{
+          color: getCSS('--cor-primaria'),
+          family: getCSS('--font'),
+          size: 30
+        }
+      }
     }
-  ]
-  
-const grafico = document.createElement('div');
-grafico.className = 'grafico';
-document.getElementById('graficos-container').appendChild(grafico);
-Plotly.newPlot(grafico,infos);
+
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    Plotly.newPlot(grafico,infos,layout);
 }
 
 quantidadeDeUsuarios();
